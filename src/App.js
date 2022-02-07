f4239105c2b25d8575990bd81a4d91cfcaddb451
arjunsharma18 <arjun.sharmaa2019@gmail.com>
2022-02-07 11:52:34 +0530

    add products frontend ready

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
 import { Home } from './components/Home';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { AddProducts } from './components/AddProducts';
 
 export default class App extends Component {
   render() {
     return <div>
-      <Home />
+      <BrowserRouter>
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route path='/addproducts' component={AddProducts} />
+        </Switch>
+      </BrowserRouter>
+
     </div>;
   }
 }
---
+++ src/components/AddProducts.js
@@ -0,0 +1,20 @@
+
+import React from 'react';
+import '../css/addproducts.css'
+
+export const AddProducts = () => {
+    return <div>
+        <div className='container' >
+            <h1>ADD PRODUCTS</h1>
+            <hr></hr>
+            <label for="exampleFormControlInput1" class="form-label">Product Name</label>
+            <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Name.." />
+            <label for="exampleFormControlInput1" class="form-label">Product Price</label>
+            <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Price.." />
+            <label for="formFile" class="form-label">Add Your Product Image</label>
+            <input class="form-control" type="file" id="formFile"></input>
+            <div className='lower-btn'></div>
+            <button type="button" class="btn btn-success">Add</button>
+        </div>
+    </div>;
+};
